2fc84bcd534a70584d6be436ecdb3a0381c37623
Marco Luciano <maurelio.sluciano@gmail.com>
2022-03-15 01:51:47 -0300

    start animations

--- js/script.js
+++ js/script.js
@@ -1,12 +1,73 @@
 const btnMenuMobile = document.querySelector('.menu-mobile-icon');
 const menuMobile = document.querySelector('.menu-mobile');
+const back_to_top = document.querySelector('.back-to-top');
+const arrow_down = document.querySelector('.arrow-down');
+// const scroll_section = document.querySelector('#skills-section');
 
 btnMenuMobile.addEventListener('click', () => {
-    if(menuMobile.classList.contains('hidden-menu')) {
-        menuMobile.classList.remove('hidden-menu');
-        menuMobile.classList.add('show-menu');
+    if(menuMobile.classList.contains('menu-closed')) {
+            menuMobile.classList.remove('menu-closed');
+            menuMobile.classList.add('menu-opened');
+        } else {
+            menuMobile.classList.add('menu-closed');
+            menuMobile.classList.remove('menu-opened');
+    }
+    btnMenuMobile.setAttibute('src', '../assets/img/close-icon.svg')
+})
+
+
+back_to_top.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+})
+
+
+addEventListener('scroll', () => {
+    if(window.scrollY == 0){
+        back_to_top.style.display = 'none'
     } else {
-        menuMobile.classList.add('hidden-menu');
-        menuMobile.classList.remove('show-menu');
+        back_to_top.style.display = 'flex'
     }
 })
+
+
+arrow_down.addEventListener('click', () => {
+    window.scrollTo({
+        top: 750,
+        left: 0,
+        behavior: 'smooth'
+    })
+})
+
+addEventListener('scroll', () => {
+    if(window.scrollY == 750){
+        arrow_down.style.display = 'none'
+    } else {
+        arrow_down.style.display = 'flex'
+    }
+})
+
+
+// scroll_section.addEventListener('scroll', () => {
+//     window.scrollLeft
+// })
+
+
+
+
+
+// btnMenuMobile.addEventListener('click', () => {
+//     if(menuMobile.style.marginLeft == '-200px') {
+//         menuMobile.style.marginLeft == '0'
+//     } else {
+//         menuMobile.style.marginLeft == '-200px'
+//     }
+//     // if(menuMobile.classList.contains('menu-opened') == true) {
+//     //     menuMobile.classList.remove('menu-opened');
+//     // } else {
+//     //     menuMobile.classList.add('menu-opened')
+//     // }
+// })
