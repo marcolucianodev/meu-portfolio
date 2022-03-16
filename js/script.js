a4733b612d0056f4ef791f93383dc34d9181c0bc
Marco Luciano <maurelio.sluciano@gmail.com>
2022-03-16 19:34:18 -0300

    finish animations

--- js/script.js
+++ js/script.js
@@ -50,18 +50,3 @@ addEventListener('scroll', () => {
 })
 
 
-const obeserver = new IntersectionObserver(entries => {
-    console.log(entries)
-
-    Array.from(entries).forEach(entry => {
-        if(entry.intersectionRatio >= 1) {
-            entry.target.classList.add('init-hidden-off')
-        }
-    })
-}, {
-    threshold: [0, .5, 1]
-})
-
-Array.from(document.querySelectorAll('.init-hidden')).forEach(element => {
-    obeserver.observe(element);
-})
